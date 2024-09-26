1ff67122de96e06a9431669bba62ab63857b4e7a
IanTheDew <ian.dewey@icloud.com>
2024-09-26 15:22:19 -0400

    Add files via upload

---
+++ ProjectBiome/src/ProjectBiome.java
@@ -0,0 +1,12 @@
+
+
+public class ProjectBiome{
+
+    public void lobby(){
+        
+    }
+    public static void main (String[] args){
+        
+        System.out.println("Hello world!");
+    }
+}
