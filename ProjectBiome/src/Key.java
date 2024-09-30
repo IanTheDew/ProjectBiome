c67fa8d4d1db7462b2bc8545840576182f71ecab
shalomiscoding <159477528+shalomiscoding@users.noreply.github.com>
2024-09-30 09:41:16 -0400

    Create Key.java
    empty key class

---
+++ ProjectBiome/src/Key.java
@@ -0,0 +1,12 @@
+public class Key {
+
+    private String keyName;
+
+    public Key(String keyName) {
+        this.keyName = keyName;
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+}
