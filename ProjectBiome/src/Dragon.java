99d3e3df29bd88d6d666b4fbad3283f821c96ac5
shalomiscoding <159477528+shalomiscoding@users.noreply.github.com>
2024-09-30 09:41:45 -0400

    Create Dragon.java
    empty dragon class

---
+++ ProjectBiome/src/Dragon.java
@@ -0,0 +1,18 @@
+public class Dragon {
+
+    private String name;
+    private int health;
+
+    public Dragon(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public void attackPlayer(Player player) {
+        // Attack player logic
+    }
+
+    public void takeDamage(int damage) {
+        // Dragon takes damage
+    }
+}
