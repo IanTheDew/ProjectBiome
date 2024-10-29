fb98c5da9488235ea0095f573b252299cf386f71
IanTheDew <ian.dewey@icloud.com>
2024-10-29 15:06:38 -0400

    Update App.java
    added gameTime

--- ProjectBiome/src/App.java
+++ ProjectBiome/src/App.java
@@ -1,11 +1,24 @@
 public class App {
 
+    boolean gameOver = false;
+    long gameTime;
     public void startGame() {
         // Game loop and main game logic
+
+        //get game start time
+        gameTime = System.nanoTime();
+
+        //main game loop
+        while (!gameOver) {
+            
+        }
     }
 
     public void endGame() {
         // End game sequence
+        //get total game time
+        gameTime = System.nanoTime() - gameTime;
+        System.out.println("Thank you for playing. \nTotal game time: " + gameTime);
     }
 
     public void displayMenu() {
