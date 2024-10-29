c0251b3c6bfeb55e5985dd1e4df6d53ba484f322
IanTheDew <ian.dewey@icloud.com>
2024-10-29 15:48:58 -0400

    Update jungleRoom.java

--- ProjectBiome/src/jungleRoom.java
+++ ProjectBiome/src/jungleRoom.java
@@ -1,11 +1,5 @@
 public abstract class jungleRoom implements Rooms {
-    private boolean isLocked;
     private String description = " see an expansive jungle...";
-
-    public Room(String description, Boolean isLocked) {
-        this.description = description;
-        this.isLocked = isLocked;
-    }
 
     public void puzzle() {
         //puzzle game
