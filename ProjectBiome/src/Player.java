16e09d6bf73b8084a5f259a528fb61848222d168
will1519 <will1519@miamioh.edu>
2024-10-29 15:40:05 -0400

    Update Player.java

--- ProjectBiome/src/Player.java
+++ ProjectBiome/src/Player.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Player {
 
     private String playerName;
-    ArrayList<String> invintory;
+    ArrayList<String> inventory;
 
 
     public Player(String playerName) {
         this.playerName = playerName;
-        invintory = new ArrayList<>();
+        inventory = new ArrayList<>();
     }
 
     public String getPlayerName() {
