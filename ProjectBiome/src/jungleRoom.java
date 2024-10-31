1e7cf38393e36ea9b4b5b9a2e815e91278e54d1d
will1519 <will1519@miamioh.edu>
2024-10-31 15:23:03 -0400

    Update jungleRoom.java

--- ProjectBiome/src/jungleRoom.java
+++ ProjectBiome/src/jungleRoom.java
@@ -1,8 +1,16 @@
-public abstract class jungleRoom implements Rooms {
+import java.util.scanner;
+
+public class jungleRoom implements Rooms {
     private String description = " see an expansive jungle...";
 
-    public void puzzle() {
+    public void puzzle(Scanner scanner) {
         //puzzle game
+        System.out.println("You are in a puzzle, enter y if you want to complete the puzzle");
+        if (scanner.nextLine().equals("y")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
