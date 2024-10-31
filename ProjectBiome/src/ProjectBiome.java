e6edd63271b574d84283ea4d43ae13d78861d61a
IanTheDew <ian.dewey@icloud.com>
2024-10-31 14:58:22 -0400

    Update ProjectBiome.java
    changed main loop to be in main method

--- ProjectBiome/src/ProjectBiome.java
+++ ProjectBiome/src/ProjectBiome.java
@@ -5,37 +5,38 @@ public class ProjectBiome{
         private boolean gameOver = false;
         private long gameTime;
 
-        
+        // create player object with inputted name
+        Scanner scanner = new Scanner(System.in);
 
+        //This is main game loop
+        do {
+
+            //Switch case for user options/inputs
+            
+        } while (!gameOver);
         
     }    
     public void startGame() {
         //get game start time
         gameTime = System.nanoTime();
 
-        //main game loop
-        do {
-            System.out.println("╔══════════════════════════════════╗");
-            System.out.println("║          Welcome to              ║");
-            System.out.println("║         Project Biome!           ║");
-            System.out.println("╚══════════════════════════════════╝\n");
-            System.out.println("Hello traveler, what is your name?: ");
+        //print main display
+        System.out.println("╔══════════════════════════════════╗");
+        System.out.println("║          Welcome to              ║");
+        System.out.println("║         Project Biome!           ║");
+        System.out.println("╚══════════════════════════════════╝\n");
+        System.out.println("Hello traveler, what is your name?: ");
             
-            // create player object with inputted name
-            Scanner scanner = new Scanner(System.in);
-            String name = scanner.nextLine();
-            Player player = new Player(name);
-            
-            // moving on...
-
-            
-            
-
-        } while(!gameOver);
+        //get user name and make player object
+        String name = scanner.nextLine();
+        Player player = new Player(name);
+        
     }
 
     public void endGame() {
         // End game sequence
+        gameOver = false;
+        
         //get total game time
         gameTime = System.nanoTime() - gameTime;
         System.out.println("Thank you for playing. \nTotal game time: " + gameTime);
