a256407acdfd1e9554974fc12ae6c256ffd65ba1
will1519 <will1519@miamioh.edu>
2024-10-31 15:17:20 -0400

    Update lobbyRoom.java

--- ProjectBiome/src/lobbyRoom.java
+++ ProjectBiome/src/lobbyRoom.java
@@ -1,5 +1,5 @@
 public abstract class lobbyRoom implements Rooms {
-    private String description = "you see a long hallway with doors...";
+    private String description = "you see a long hallway with three doors and a large gate requiring three keys...";
 
     public void puzzle() {
         //puzzle game
