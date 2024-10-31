1b7616b6470a82557413b1977f6f0e10a5629328
will1519 <will1519@miamioh.edu>
2024-10-31 15:22:28 -0400

    Update finalRoom.java

--- ProjectBiome/src/finalRoom.java
+++ ProjectBiome/src/finalRoom.java
@@ -1,8 +1,16 @@
-public abstract class finalRoom implements Rooms {
+import java.util.scanner;
+
+public class finalRoom implements Rooms {
     private String description = " a dragon emerges from the shadows...";
 
-    public void puzzle() {
+    public void puzzle(Scanner scanner) {
         //puzzle game
+        System.out.println("You are in a puzzle, enter y if you want to complete the puzzle");
+        if (scanner.nextLine().equals("y")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
