6a16fc568aa4b9f547df0d7a7751d5fda3c0a623
will1519 <will1519@miamioh.edu>
2024-10-31 15:24:08 -0400

    Update secretRoom.java

--- ProjectBiome/src/secretRoom.java
+++ ProjectBiome/src/secretRoom.java
@@ -1,10 +1,18 @@
-public abstract class secretRoom implements Rooms {
+import java.util.scanner;
+
+public class secretRoom implements Rooms {
     private String description = " you fall into a hidden gold cache...";
 
-    public void puzzle() {
+    public void puzzle(Scanner scanner) {
         //puzzle game
+        System.out.println("You are in a puzzle, enter y if you want to complete the puzzle");
+        if (scanner.nextLine().equals("y")) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
+    
     @Override
     public void enterRoom() {
         // Room entering logic
