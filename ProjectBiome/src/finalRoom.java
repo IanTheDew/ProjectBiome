5008120a2bfe8e3afb92cb85051bbe783d19b916
will1519 <will1519@miamioh.edu>
2024-11-02 14:39:45 -0400

    Update finalRoom.java

--- ProjectBiome/src/finalRoom.java
+++ ProjectBiome/src/finalRoom.java
@@ -1,17 +1,27 @@
-import java.util.scanner;
+import java.util.Scanner;
 
-public class finalRoom implements Rooms {
+public class finalRoom {
     
-    @Override
-    public static void enterRoom(Scanner scanner) {
+	private static void roomActivity(Player player, Scanner scanner) {
+        System.out.println("You are in the final boss fight, would you like to win? (y)");
+        if (scanner.nextLine().equals("y")) {
+            System.out.println("Congrats! You have beaten Project Biome!");
+        } else {
+        	System.out.println(player.getPlayerName() + " awakes in the lobby room after being defeated by the dragon");
+            exitRoom(player, scanner);
+        }
+    }
+	
+    public static void enterRoom(Player player, Scanner scanner) {
         System.out.println(" a dragon emerges from the shadows...");
+        boolean validInput;
         do {
-            boolean validInput = true;
-            System.out.println("Would you like to continue to the boss fight (b)?");
+            validInput = true;
+            System.out.println("Would you like to continue to the boss fight (b)!??!? There is no backing out now!");
             String userInput = scanner.nextLine();
         
-            if (userInput.equals("b")) [
-                roomActivity();
+            if (userInput.equals("b")) {
+                roomActivity(player, scanner);
             } else {
                 System.out.println("Your input was not recognized, please try again");
                 validInput = false;
@@ -20,21 +30,8 @@ public class finalRoom implements Rooms {
         } while (!validInput);
     }
 
-    @Override
-    private static void roomActivity(Scanner scanner) {
-        //puzzle game
-        System.out.println("You are in the final boss fight, would you like to win? (y)");
-        if (scanner.nextLine().equals("y")) {
-            System.out.println("Congrats! You have beaten Project Biome!");
-            endGame();
-        } else {
-            exitRoom();
-        }
-    }
-
-    @Override
-    private void exitRoom(Scanner scanner) {
-        lobbyRoom.enterRoom(scanner);
+    private static void exitRoom(Player player, Scanner scanner) {
+        lobbyRoom.enterRoom(player, scanner);
     }
         
     
