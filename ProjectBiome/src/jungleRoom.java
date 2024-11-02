d4f03475705c236e12674360be8502a2f6a5c30b
will1519 <will1519@miamioh.edu>
2024-11-02 14:38:37 -0400

    Update jungleRoom.java

--- ProjectBiome/src/jungleRoom.java
+++ ProjectBiome/src/jungleRoom.java
@@ -1,26 +1,57 @@
-import java.util.scanner;
+import java.util.Scanner;
 
-public class jungleRoom implements Rooms {
-    private String description = " see an expansive jungle...";
+public class jungleRoom {
 
-    public void puzzle(Scanner scanner) {
-        //puzzle game
-        System.out.println("You are in a puzzle, enter y if you wish to successfully complete the puzzle");
+    public static void roomActivity(Player player, Scanner scanner) {
+    	System.out.println("You are in a puzzle, will you successfully solve the puzzle (y)?");
         if (scanner.nextLine().equals("y")) {
-            return true;
+        	System.out.println("Congrats! You have solved the puzzle and obtained the Jungle Key!");
+        	player.collectItem("Jungle Key");
+        	exitRoom(player, scanner);
+        	
         } else {
-            return false;
+        	
+        	boolean validInput;
+        	
+        	do {
+        		validInput = true;
+	            System.out.println("You have failed the puzzle! Would you like to retry (r) or exit back to the lobby (n)?");
+	            String input = scanner.nextLine();
+	            if (input.equals("r")) {
+	            	roomActivity(player, scanner);
+	            } else if (input.equals("n")) {
+	            	exitRoom(player, scanner);
+	            } else {
+	            	System.out.println("Your input was not recognized, please try again");
+	            	validInput = false;
+	            }
+        	} while (!validInput);
         }
     }
-
-    @Override
-    public void enterRoom() {
-        // Room entering logic
+    
+    public static void enterRoom(Player player, Scanner scanner) {
+        System.out.println(" see an expansive jungle...");
+        boolean validInput;
+        do {
+        	validInput = true;
+            System.out.println("Would you like to leave this room (l) or continue to the puzzle (p)?");
+            String userInput = scanner.nextLine();
+            
+            if (userInput.equals("l")) {
+                exitRoom(player, scanner);
+            } else if (userInput.equals("p")) {
+                roomActivity(player, scanner);
+            } else {
+                System.out.println("Your input was not recognized, please try again");
+                validInput = false;
+            }
+        } while (!validInput);
+        
     }
 
-    @Override
-    public void exitRoom() {
-        // Room exiting logic
+
+    public static void exitRoom(Player player, Scanner scanner) {
+        lobbyRoom.enterRoom(player, scanner);
     }
     
 }
