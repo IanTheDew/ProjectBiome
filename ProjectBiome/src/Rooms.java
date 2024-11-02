d8e30bef55931e925b7bbc179ef9aa2b6fdbfce2
IanTheDew <ian.dewey@icloud.com>
2024-11-02 17:34:46 -0400

    Create Rooms.java
    abstract class for consistency

---
+++ ProjectBiome/src/Rooms.java
@@ -0,0 +1,17 @@
+/*
+ * This code is a parent class for all of the rooms
+ * Functionally it acts similar to an interface,
+ * due to its abstract modifier and empty method bodies
+ * 
+ * Since it is not an interface, we can call on the methods in the subclasses 
+ * without creating instances of them (something we cannot do with an interface)
+ * 
+ * @Author CSE 201 Group A
+ */
+import java.util.Scanner;
+
+public abstract class Rooms {
+    public static void enterRoom(Player player, Scanner scanner) {}
+    public static void exitRoom(Player player, Scanner scanner) {}
+    public static void puzzle(Player player, Scanner scanner) {}
+}
