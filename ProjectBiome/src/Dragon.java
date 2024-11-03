9acf8b7a159863eb3b65f48bcb3d6cd784ee8f89
madeline3 <swisher.madeline@gmail.com>
2024-11-03 17:19:50 -0500

    Update Dragon.java

--- ProjectBiome/src/Dragon.java
+++ ProjectBiome/src/Dragon.java
@@ -8,10 +8,18 @@ public class Dragon {
         this.health = health;
     }
 
+    /**
+    @param player that recieves damage
+    Description: THe dragon attacks the player and reduces the players health
+    **/
     public void attackPlayer(Player player) {
         // Attack player logic
     }
 
+   /**
+   @param damage: the amount of damage inflicted
+   Description: Reduces the health by how much damage the dragon took
+    **/
     public void takeDamage(int damage) {
         // Dragon takes damage
     }
