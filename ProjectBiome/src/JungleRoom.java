fa95e85f2435d73ea05b8a89521b9086e6fd5d20
sheasully16 <sulli369@miamioh.edu>
2024-11-19 15:46:46 -0500

    Update JungleRoom.java
    this is my version (shea) of jungle room. everything implemented but I can change

--- ProjectBiome/src/JungleRoom.java
+++ ProjectBiome/src/JungleRoom.java
@@ -1,65 +1,86 @@
-import java.util.Scanner;
-import java.util.Random;
-
-public class JungleRoom extends Rooms 
-{
-	private static boolean puzzleSolved = false;
-	Random rand = new Random();
+// THIS IS RANDOM GENERATOR CODE I MIGHT USE IN THE FUTURE - shea
+// private static boolean puzzleSolved = false;
+// Random rand = new Random();
 	
+// int endCond = 0; // once this reaches 3, the puzzle is complete
+// Random rand = new Random();
+// int rand_roll = rand.nextInt(3) + 1;
+// while(endCond != 3)
+// {
+// 	if(rand_roll == 1 || rand_roll == 3)
+// 	{
+// 		System.out.print("You missed. How unfortunate.");
+// 	}
+// 	if(rand_roll == 2)
+// 	{
+// 		endCond++;
+// 		System.out.print("Hit! Good going, " + player.getPlayerName() + ". ");
+// 		puzzleSolved = true;
+// 	}
+// }
+import java.util.Scanner;
+
+public class JungleRoom extends Rooms {
 	/**
       	@param player navigating the room
         @param scanner to take input
 	Description: Room and puzzles are navigated
 	**/
-    public static void roomActivity(Player player, Scanner scanner) 
-	{
-    	System.out.println("You are in a puzzle, will you attempt to solve the puzzle (y)?");
-        if (scanner.nextLine().equals("y")) 
-	{
-		System.out.println("There are enemies in front of you. Attack?");
-		if (scanner.nextLine().equalsIgnoreCase("y"))
-		   {
-			int endCond = 0; // once this reaches 3, the puzzle is complete
-			Random rand = new Random();
-			int rand_roll = rand.nextInt(3) + 1;
-			while(endCond != 3)
-			{
-				if(rand_roll == 1 || rand_roll == 3)
-				{
-					System.out.print("You missed. How unfortunate.");
-				}
-				if(rand_roll == 2)
-				{
-					endCond++;
-					System.out.print("Hit! Good going, " + player.getPlayerName() + ". ");
-					puzzleSolved = true;
-				}
-			}
-		   }
+    public static void roomActivity(Player player, Scanner scanner) {
+    	
+	boolean complete = false; // if room was completed
+	char choice;
+	
+	do {
+		displayMenu();
+		choice = scanner.nextLine().charAt(0);
+	    switch (choice) {
+		case 'F':
+		     // do room activity
+		    System.out.println("You engage the goblins, they look at you menacingly");
+		    System.out.println("With your sword drawn, you feel a surge of confidence. The goblin leader sneers, but you stand your ground.");
+		    System.out.println("The other goblins hesitate, clearly intimidated by your weapon. Do you:");
+		    System.out.println("1. Charge at the goblins with a fierce battle cry! (press C)");
+		    System.out.println("2. Attempt to negotiate with the goblins, offering them treasure for safe passage. (press N)");
+		    choice = scanner.nextLine().charAt(0);
+		    switch (choice) {
+			case 'C':
+			    System.out.println("You charge at the goblins with a battle cry! They scatter in fear, throwing they key at you as they run away.");
+				player.collectItem("Jungle Key");
+			    complete = true;
+			    break;
+			case 'N':
+			    System.out.println("You offer the goblins some treasure. They consider your proposal and ultimately trade their key for it.");
+				player.collectItem("Jungle Key");
+			    complete = true;
+			    break;
+			default:
+			    System.out.println("Confused, you hesitate, and the goblins take the opportunity to surround you!");
+			    break;
+		    }
+		    break;
+		case 'L':
+			// leave the room
+			System.out.println("You spot a nearby bush and make a break for it! You escape into the jungle, leaving the goblins behind.");
+			complete = true;
+		    break;
+		case 'O':
+			// get keys from inventory
+		    System.out.println("Opening inventory...");
+		    player.getInventory();
+		    break;
+		case 'Q':
+			// quit the game (in reality just exits the room)
+		    System.out.println("Quitting the game...");
+		    complete = true;
+		    break;
+		default:
+		    System.out.println("Input not recognized, please try again.");
+	    }
+	} while (!complete);
 		
-        	System.out.print("The enemy is down. There is a key around its neck. You take it.");
-        	player.collectItem("Jungle Key");
-		System.out.print("The Jungle Key is in your possession and you return to the lobby.");
-        	exitRoom(player, scanner);
-        	
-        } else {
-        	
-        	boolean validInput;
-        	
-        	do {
-        		validInput = true;
-	            System.out.println("Would you like to retry (r) or exit back to the lobby (n)?");
-	            String input = scanner.nextLine();
-	            if (input.equalsIgnoreCase("r")) {
-	            	roomActivity(player, scanner);
-	            } else if (input.equalsIgnoreCase("n")) {
-	            	exitRoom(player, scanner);
-	            } else {
-	            	System.out.println("Your input was not recognized, please try again");
-	            	validInput = false;
-	            }
-        	} while (!validInput);
-        }
+		// exit the room if complete
+    	exitRoom(player, scanner);
     }
     /**
       	@param player navigating the jungle room
@@ -67,12 +88,22 @@ public class JungleRoom extends Rooms
 	Description: Prompts for user input when entering the jungle room
 	**/
     public static void enterRoom(Player player, Scanner scanner) {
-        System.out.println(" see an expansive jungle...");
+    	// check if player already has the key..
+    	if (player.checkKey("Jungle Key")) {
+		System.out.println("You already have the jungle key!");
+		exitRoom(player, scanner);
+    	}
+    	
+    	// print a description of the room
+    	System.out.println("You walk through the green door into a large open and lush jungle. \n" +
+	    "You encounter a group of green goblins where you can see \n"
+	    + "the leader has the green key hanging from a necklace around their neck.\n");
+    	
         boolean validInput;
         do {
         	validInput = true;
-            System.out.println("Would you like to leave this room (l) or continue to the puzzle (p)?");
-            String userInput = scanner.nextLine();
+	    System.out.println("Would you like to leave this room (l) or engage the goblins (p)?");
+	    String userInput = scanner.nextLine();
             
             if (userInput.equalsIgnoreCase("l")) {
                 exitRoom(player, scanner);
@@ -83,16 +114,31 @@ public class JungleRoom extends Rooms
                 validInput = false;
             }
         } while (!validInput);
-        
     }
 
-/**
+    /**
       	@param player navigating the jungle room
         @param scanner to take input
 	Description: Prompts for user input when exiting the jungle room
 	**/
     public static void exitRoom(Player player, Scanner scanner) {
+    	System.out.println("Player '" + player.getPlayerName() + "' is leaving the Jungle Room");
         LobbyRoom.enterRoom(player, scanner);
     }
     
+    // provide the user with list of things they can do
+    public static void displayMenu() {
+    	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("                ~*~ Jungle Encounter ~*~                 ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("                  What will you do?:                     ");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("|  1. Fight the goblins... (press F)                     |");
+        System.out.println("|  2. Flee... (press L)                                  |");
+        System.out.println("|  3. Open Your Inventory... (press O)                   |");
+        System.out.println("|  4. Quit the game... (press Q)                         |");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.print("Choose an option: ");
+    }
 }
