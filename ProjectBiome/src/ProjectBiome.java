c2945613e3926fb5c28ce44b1f1907dd9e934c9e
will1519 <will1519@miamioh.edu>
2024-12-02 18:56:14 -0500

    Update ProjectBiome.java

--- ProjectBiome/src/ProjectBiome.java
+++ ProjectBiome/src/ProjectBiome.java
@@ -13,14 +13,14 @@ import java.util.Scanner;
 
 public class ProjectBiome{
 	
-
     public static void main (String[] args){
     	long gameTime = startGame();
     	endGame(gameTime);
-    }    
+    }  
+	
     /**
     Description: starts the game and collects the time it takes the player to finish, entering the lobby room, and prints the welcome prompt
-     **/
+    **/
     public static long startGame() {
     	// Set up initial game state
         long gameTime = System.nanoTime();
@@ -47,7 +47,7 @@ public class ProjectBiome{
     
     /**
     Description: ends the game, prints the total playtime, and prompts the end message
-     **/
+    **/
     public static void endGame(long gameTime) {
         //get total game time
         gameTime = System.nanoTime() - gameTime;
